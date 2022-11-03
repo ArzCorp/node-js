4a87c719843a7ec6261b663ef609b453abb014af
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-02 20:01:17 -0600

    💫 added module path

--- src/path.js
+++ src/path.js
@@ -1,2 +1,7 @@
-console.log(__dirname)
-console.log(__filename)
+const path = require('path')
+
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(path.sep)
+// console.log(path.join('/content', 'subfolder', 'test.txt'))
+console.log(path.basename('/home/users/projects/node-js/index.txt'))
