0884ebc4172323a94a3166663aace3885ead97f9
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-03 21:40:07 -0600

    ✨ added file sistem

--- src/path.js
+++ src/path.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
+console.log(path.resolve('index.js'))
 // console.log(__dirname)
 // console.log(__filename)
 // console.log(path.sep)
 // console.log(path.join('/content', 'subfolder', 'test.txt'))
-console.log(path.basename('/home/users/projects/node-js/index.txt'))
+// console.log(path.basename('/home/users/projects/node-js/index.txt'))
+// console.log(path.dirname("/home/users/projects/node-js/index.txt"))
