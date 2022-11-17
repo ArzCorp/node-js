56006128ce2137aa5fb3208c821991208a48c147
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-11-16 21:35:34 -0600

    💫 add fs sync

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-const { PORT } = require('./src/constants.js')
+const fs = require('fs')
 
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-	if (req.url === '/') {
-		if (req.method === 'GET') return res.end('Homw Page')
-		return res.end('Method not allowed')
-	}
-
-	return res.end('404 not found')
+fs.writeFileSync('./src/static/text.txt', ' Hello World', {
+	flag: 'a',
 })
 
-server.listen(PORT)
-
-console.log(`Server is listening on port ${PORT}`)
+const fileContent = fs.readFileSync('./src/static/text.txt', 'utf8')
+console.log(fileContent)
--- src/https.js
+++ src/https.js
@@ -1,8 +1,16 @@
+const { PORT } = require('./src/constants.js')
+
 const http = require('http')
 
-http
-	.createServer((request, response) => {
-		response.write('Hello World')
-		response.end()
-	})
-	.listen(5000)
+const server = http.createServer((req, res) => {
+	if (req.url === '/') {
+		if (req.method === 'GET') return res.end('Homw Page')
+		return res.end('Method not allowed')
+	}
+
+	return res.end('404 not found')
+})
+
+server.listen(PORT)
+
+console.log(`Server is listening on port ${PORT}`)
